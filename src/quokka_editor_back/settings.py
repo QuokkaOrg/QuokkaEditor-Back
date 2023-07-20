80520c8cfa1b9423a02c3b10e80e2473af9a2a22
D0cza <d0.czajkowski@gmail.com>
2023-07-20 20:30:37 +0200

    remove-secret-key-from-docker-compose

--- src/quokka_editor_back/settings.py
+++ src/quokka_editor_back/settings.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     rich_logging: bool = False
     root_log_level: str = "ERROR"
     log_level: str = "DEBUG"
-    secret_key: str
+    secret_key: str = "cbf2eba1064f98a3c8f3a239ae257176444cfcbebdf7c7bc038b85b409df5bfb"
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
 
